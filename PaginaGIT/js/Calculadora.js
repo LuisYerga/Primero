07f7968626444e38587071ca50c3fff2bcfb636a
Luis Yerga <lyergamayor1@gmail.com>
2023-09-25 13:51:50 +0200

    pagina web2

---
+++ PaginaGIT/js/Calculadora.js
@@ -0,0 +1,19 @@
+"use strict";
+
+function appendToDisplay(value) {
+    document.getElementById('pantalla').value += value;
+}
+
+function clearDisplay() {
+    document.getElementById('pantalla').value = '';
+}
+
+function calculateResult() {
+    try {
+        const expression = document.getElementById('pantalla').value;
+        const result = eval(expression);
+        document.getElementById('pantalla').value = result;
+    } catch (error) {
+        document.getElementById('pantalla').value = 'Error';
+    }
+}
